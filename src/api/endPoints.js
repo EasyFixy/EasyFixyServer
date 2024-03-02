b0c927a3de04d1f6f4da69fe1591c04ce82fd7a5
juan esteban <jcarranza@unal.edu.co>
2024-03-02 12:08:07 -0500

    borrar test

--- src/api/endPoints.js
+++ src/api/endPoints.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { ping } = require('../controllers/pingController');
+
 const { userRegistration } =require('../controllers/userRegistration')
 const { userLogin } = require('../controllers/userLogin')
 
-router.get('/ping', ping);
 router.get('/userRegistration', userRegistration);
 router.get('/userLogin', userLogin);
 
