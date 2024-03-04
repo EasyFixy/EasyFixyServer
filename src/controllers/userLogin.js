07bf306d4862f08a3e83b9b1845df23414ab0efd
juan esteban <jcarranza@unal.edu.co>
2024-03-04 17:34:27 -0500

    se terminan login y registro

--- src/controllers/userLogin.js
+++ src/controllers/userLogin.js
@@ -2,30 +2,54 @@ const dbConnection = require('../db/dbConnection')
 const jwt = require('jsonwebtoken')
 
 module.exports.userLogin = (req, res) => {
-    
+
     const email = req.query.email
     const pass = req.query.password
 
     const consulta = "SELECT IDUSER FROM users WHERE user_email = ? and user_password = ? limit 1"
 
-    try {
-        dbConnection.query(consulta,[email, pass], (err, results) => {
-            if(err){
-                res.send(err)
+    function validateMail(email) {
+        // Expresión regular para validar el formato del correo electrónico
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    }
+    function validateLength(string, min, max) {
+        return string.length >= min && string.length <= max
+    }
+    function validateSpecialChars(string) {
+        const caracteresEspeciales = ".-_,;{}´¨+-*/!$%&#?¿'";
+        for (let i = 0; i < caracteresEspeciales.length; i++) {
+            if (string.includes(caracteresEspeciales[i])) {
+                return true;
             }
-            if(results.length > 0){
-                userId = results[0].IDUSER
-                const token = jwt.sign({ userId }, "Stack",{
-                    expiresIn: '2d'
-                });
-                console.log(results[0].IDUSER)
-                res.json({token: token})
-            }else {
-                res.json("wrong user")
-            }
-            
-        })
-    } catch (e) {
-        console.log("error")
+        }
+
+        return false; // Si ninguno de los caracteres especiales está presente
+    }
+
+    if (validateMail(email) && validateLength(pass,1,80) && validateSpecialChars(pass)) {
+        try {
+            dbConnection.query(consulta, [email, pass], (err, results) => {
+                if (err) {
+                    res.send(err)
+                }
+                if (results.length > 0) {
+                    userId = results[0].IDUSER
+                    const token = jwt.sign({ userId }, "Stack", {
+                        expiresIn: '2d'
+                    });
+                    console.log(results[0].IDUSER)
+                    res.json({statusCode: 200, message:"en la buena pai",token: token })
+                } else {
+                    res.json({statusCode: 400, message:"wrong user/password"})
+                }
+
+            })
+        } catch (e) {
+            console.log("error")
+        }
+    }
+    else {
+        res.json({statusCode: 400, message:"wrong user/password"})
     }
 }
--- src/controllers/userRegistration.js
+++ src/controllers/userRegistration.js
@@ -2,20 +2,65 @@ const dbConnection = require('../db/dbConnection')
 
 module.exports.userRegistration = (req, res) => {
 
-    const name = req.query.name 
+    const name = req.query.name
     const email = req.query.email
-    const pass = req.query.password
+    const password = req.query.password
     const phoneNumber = req.query.phoneNumber
     const user_national_id = req.query.user_national_id
     const user_kind_account = req.query.user_kind_account
 
-    const consulta = "INSERT INTO `users` (`user_name`, `user_email`, `user_password`, `user_phone_number`, `user_national_id`, `user_kind_account`) VALUES (?, ?, ?, ?, ?, ?);"
+    const consultaInsertarUsuario = "INSERT INTO `users` (`user_name`, `user_email`, `user_password`, `user_phone_number`, `user_national_id`, `user_kind_account`) VALUES (?, ?, ?, ?, ?, ?);"
+    const consultaVerificarMail = "SELECT count(iduser) as cantidad FROM prueva_easyfixy.users where user_email = ?;"
 
-    try {
-        dbConnection.query(consulta,[name, email, pass, phoneNumber, user_national_id, user_kind_account], (err, results) => {
-            res.send(results)
-        })
-    } catch (e) {
-        console.log("error")
+    function validateMail(email) {
+        // Expresión regular para validar el formato del correo electrónico
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    }
+
+    function alreadyNotExistMail(email) {
+        try {
+            dbConnection.query(consultaVerificarMail, [email], (err, results) => {
+                
+                if(results[0].cantidad <= 0){ // si el usuario no existe en la db
+                    try {
+                        dbConnection.query(consultaInsertarUsuario, [name, email, password, phoneNumber, user_national_id, user_kind_account], (err, results) => {
+                            res.send({ statusCode: 200, data: results })
+                        })
+                    } catch (e) {
+                        console.log({ statusCode: 400, message: "error en db" })
+                        res.send({ statusCode: 400, message: "error en db" })
+                    }
+                }else {
+                    res.send({ statusCode: 400, message: "user already exists" })
+                }
+            
+            })
+        } catch (e) {
+            console.log({ statusCode: 400, message: "error en db" })
+            res.send({ statusCode: 400, message: "error en db" })
+        }
+    }
+
+    function validateLength(string, min, max) {
+        return string.length >= min && string.length <= max
+    }
+
+    function validateSpecialChars(string) {
+        const caracteresEspeciales = ".-_,;{}´¨+-*/!$%&#?¿'";
+        for (let i = 0; i < caracteresEspeciales.length; i++) {
+            if (string.includes(caracteresEspeciales[i])) {
+                return true;
+            }
+        }
+
+        return false; // Si ninguno de los caracteres especiales está presente
+    }
+    
+    if (validateMail(email) && validateLength(password, 1, 80) && validateSpecialChars(password)) {
+        alreadyNotExistMail(email)
+    }
+    else {
+        res.json({ statusCode: 400, message: "wrong user/password" })
     }
 }
