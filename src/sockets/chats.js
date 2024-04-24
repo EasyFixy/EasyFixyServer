87a12bb973f97ae22f1b4aed7382f0632e640f30
luisda <lusanchezmo@unal.edu.co>
2024-04-24 10:27:21 -0500

    clientspersocket

--- src/sockets/chats.js
+++ src/sockets/chats.js
@@ -2,7 +2,7 @@ const dbConnection = require('../db/dbConnection')
 const SQLScripts = require('../db/SQLScripts')
 require('dotenv').config({ path: './../.env' })
 
-const clientsPerSocket = {}
+const clientsPerSocket = {} // {userId:"SOCKET_ID", 7:"asdasdwqe", 1:"OJDWKENC"}
 const consultaInertMessage = SQLScripts.scriptInsertMessage
 const consultaInsertRelationShip = SQLScripts.scriptInsertUserMessageRelationship
 
@@ -44,7 +44,9 @@ module.exports.chats =  (io) => {
     return async (socket) => {
         console.log('a user has connected! ', socket.handshake.auth.userId, socket.id)
         clientsPerSocket[socket.handshake.auth.userId] = socket.id
+        console.log("Clientes que se encuentran en el socket",clientsPerSocket);
         socket.on('disconnect', () => {
+            delete clientsPerSocket[socket.handshake.auth.userId]; // <-- quitar del objeto el usuario cuando se desconecta
             console.log('an user has disconnected', socket.handshake.auth.userId)
         })
 
